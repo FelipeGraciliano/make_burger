289e751cdcb64de401f82c43f5676f49f372dfb2
felipe graciliano <felipeegraciliano@gmail.com>
2024-04-19 15:07:52 -0300

    fix Navbar and Footer

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,44 @@
 <template>
-  <Navbar />
-  <router-view/>
+  <Navbar :logo="logo_src" :alt="app_name" />
+  <router-view />
   <Footer />
 </template>
 
 <script>
-
-
-import Navbar from './components/Navbar.vue';
-import Footer from './components/Footer.vue';
+import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
   components: {
     Navbar,
-    Footer
-  }
-}
+    Footer,
+  },
+  data() {
+    return {
+      logo_src: "/img/logo.png",
+      app_name: "Make Your Burger",
+    };
+  },
+};
 </script>
 
 <style>
+* {
+  font-family: Helvetica;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
 
+.main-container {
+  margin: 50px;
+  min-height: 250px;
+}
+
+h1 {
+  text-align: center;
+  font-style: 24px;
+  margin-bottom: 30px;
+  color: #222;
+}
 </style>
