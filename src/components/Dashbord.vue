861904263ad050843291195cdb3df59bd5731c42
felipe graciliano <felipeegraciliano@gmail.com>
2024-04-23 19:36:35 -0300

    conclusao

--- src/components/Dashbord.vue
+++ src/components/Dashbord.vue
@@ -1,5 +1,6 @@
 <template>
     <div id="burger-table">
+        <Message :message="message" v-show="message" />
         <div>
             <div id="burger-table-heading">
                 <div class="order-id">#:</div>
@@ -11,22 +12,26 @@
             </div>
         </div>
         <div id="burger-table-rows">
-            <div class="burger-table-row">
-                <div class="order-number">1</div>
-                <div>João</div>
-                <div>Pão de trigo</div>
-                <div>Alcatra</div>
+            <div class="burger-table-row" v-for="burger in burgers" v-bind:key="burger.id">
+                <div class="order-number">{{ burger.id }}</div>
+                <div>{{ burger.nome }}</div>
+                <div>{{ burger.pao }}</div>
+                <div>{{ burger.carne }}</div>
                 <div>
                     <ul>
-                        <li>Queijo</li>
-                        <li>Salame</li>
+                        <li v-for="(opcional, index) in burger.opcionais" :key="index">
+                            {{ opcional }}
+                        </li>
                     </ul>
                 </div>
                 <div>
-                    <select name="status" class="status">
+                    <select name="status" class="status" @change="updateBurger($event, burger.id)">
                         <option value="">Selecione</option>
+                        <option v-for="s in status" :key="s.id" :value="s.tipo" :selected="burger.status == s.tipo">
+                            {{ s.tipo }}
+                        </option>
                     </select>
-                    <button class="delete-btn">Cancelar</button>
+                    <button class="delete-btn" @click="deleteBurger(burger.id)">Cancelar</button>
                 </div>
             </div>
         </div>
@@ -34,8 +39,94 @@
 </template>
 
 <script>
+
+import Message from "./Message.vue";
+
 export default {
-    name: "Dashbord"
+    name: "Dashbord",
+    data() {
+        return {
+            burgers: null,
+            burgers_id: null,
+            status: [],
+            Message: null
+        }
+    },
+    components: {
+        Message
+    },
+    methods: {
+        async getPedidos() {
+            const req = await fetch("http://localhost:3000/burgers");
+
+            const data = await req.json();
+
+            this.burgers = data;
+
+            console.log(this.burgers);
+
+            // resgatar status //
+            this.getStatus();
+
+        },
+        async getStatus() {
+            const req = await fetch("http://localhost:3000/status");
+
+            const data = await req.json();
+
+            this.status = data;
+
+            console.log(data);
+        },
+        async deleteBurger(id) {
+
+            const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+                method: "DELETE"
+            });
+
+            const res = await req.json();
+
+            // colocar uma msg de sistema
+            this.message = `Pedido removido com sucesso`;
+
+            // limpar msg
+            setTimeout(() => {
+                this.message = null;
+            }, 3000);
+
+            this.getPedidos();
+
+        },
+        async updateBurger(event, id) {
+
+            const optionn = event.target.value;
+
+            const dataJson = JSON.stringify({ status: optionn });
+
+            const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: dataJson
+            });
+
+            const res = await req.json();
+
+            // colocar uma msg de sistema
+            this.message = `O Pedido N° ${res.id} foi atualizado para ${res.status} com sucesso!`;
+
+            // limpar msg
+            setTimeout(() => {
+                this.message = null;
+            }, 3000);
+
+            console.log(res);
+        }
+    },
+    mounted() {
+        this.getPedidos();
+    }
 }
 </script>
 
